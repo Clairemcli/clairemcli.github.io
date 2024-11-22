d374e588bb9864fdbe770986911f2016f0ed66ee
Claire Li <clairemcli@ucla.edu>
2024-11-21 23:32:19 -0800

    delete readme

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/claire/Desktop/Applications/GitHub/Portfolio/venv"
+set -gx VIRTUAL_ENV "/Users/claire/Desktop/Applications/GitHub/clairemcli.github.io/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
